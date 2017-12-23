eaa53cea5204c5a0cb4fb1834604e2f2a68dff36
shenwl <shenweilun96@gmail.com>
2017-12-23 17:41:43 +0800

    add publicList

--- src/component/authRoute/authRoute.js
+++ src/component/authRoute/authRoute.js
@@ -5,6 +5,11 @@ import { withRouter } from 'react-router-dom'
 @withRouter
 class AuthRoute extends React.Component {
     componentDidMount() {
+        const publicList = ['/login', '/register']
+        const pathname = this.props.location.pathname
+        if(publicList.indexOf(pathname) > -1) {
+            return null
+        }
         //获取用户信息
         axios.get('/user/info')
             .then(res => {
@@ -12,7 +17,7 @@ class AuthRoute extends React.Component {
                     if(res.data.code==0) {
 
                     }else {
-                        console.log(this.props.history)
+                        this.props.history.push('/login')
                     }
                     console.log(res.data)
                 }
