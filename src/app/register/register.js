a61c13bd78b15bfcd4d4a47d03761e55a98fa5ba
shenwl <shenweilun96@gmail.com>
2017-12-23 22:58:04 +0800

    rm log

--- src/app/register/register.js
+++ src/app/register/register.js
@@ -3,6 +3,7 @@ import Logo from '../../component/logo/logo'
 import { List, InputItem, WingBlank, WhiteSpace, Button, Radio } from 'antd-mobile'
 import { connect } from 'react-redux'
 import { register } from '../../redux/user.redux'
+import { Redirect } from 'react-router-dom'
 
 @connect(
     state => state.user,
@@ -27,13 +28,13 @@ class Register extends React.Component {
 
     registerHandler() {
         this.props.register(this.state)
-        console.log(this.state)
     }
 
     render() {
         const RadioItem = Radio.RadioItem
         return (
             <div>
+                {this.props.redirectTo ? <Redirect to={this.props.redirectTo}></Redirect> : null}
                 <Logo></Logo>
                 <WingBlank>
                     <List>
