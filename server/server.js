15006749795e09318cfd8f09e956cbbdd904ecc0
shenwl <shenweilun96@gmail.com>
2017-12-23 16:22:46 +0800

    add user

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
-
-const DB_URL = 'mongodb://localhost:27017'
-mongoose.connect(DB_URL)
+const userRouter = require('./user')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+app.use('/user', userRouter)
 
-
-app.listen(8081, function() {
+app.listen(8081, () => {
     console.log('ojbk,请空中转体720度用电饭煲打开"http://localhost:8081"')
 })
