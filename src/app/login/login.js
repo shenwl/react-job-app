448d5eef0f9aa6185cdbc1f4643a521cfb88023a
shenwl <shenweilun96@gmail.com>
2017-12-23 23:24:06 +0800

    login

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Logo from '../../component/logo/logo'
 import { List, InputItem, WingBlank, WhiteSpace, Button } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { login } from '../../redux/user.redux'
 
+@connect(
+    state => state.user,
+    {login}
+)
 class Login extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            user: '',
+            pwd: ''
+        }
         this.register = this.register.bind(this)
+        this.loginHandler = this.loginHandler.bind(this)
+    }
+
+    changeHandler(key, value) {
+        this.setState({
+            [key]: value
+        })
+    }
+
+    loginHandler() {
+        this.props.login(this.state)
     }
 
     register() {
@@ -18,13 +39,19 @@ class Login extends React.Component {
                 <Logo></Logo>
                 <WingBlank>
                     <List>
-                        <InputItem placeholder='请输入用户名'></InputItem>
+                        <InputItem 
+                            onChange={v=>this.changeHandler('user', v)}
+                            placeholder='请输入用户名'></InputItem>
                         <WhiteSpace />
-                        <InputItem placeholder='请输入密码'></InputItem>
+                        <InputItem
+                            onChange={v=>this.changeHandler('pwd', v)}
+                            placeholder='请输入密码'></InputItem>
                     </List>
                     <WhiteSpace />
                     <WhiteSpace />
-                    <Button type='primary'>登陆</Button>
+                    <Button 
+                        onClick={this.loginHandler()}
+                        type='primary'>登录</Button>
                     <WhiteSpace />
                     <Button onClick={this.register} type='primary'>注册</Button>
                 </WingBlank>
