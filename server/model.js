dbe4c0aaa1a6e8e18359f3da8f6bafac975ff195
shenwl <shenweilun96@gmail.com>
2017-12-23 21:50:03 +0800

    add usermodel

--- server/model.js
+++ server/model.js
@@ -1,3 +1,33 @@
 const mongoose = require('mongoose')
-const DB_URL = 'mongodb://localhost:27017'
+const DB_URL = 'mongodb://localhost:27017/job-chat'
 mongoose.connect(DB_URL)
+
+const models = {
+    user: {
+        'user': {type: String, require: true},
+        'pwd': {type: String, require: true},
+        'type': {type: String, require: true},
+        'avatar': {type: String},
+        //个人简介
+        'desc': {type: String},
+        //想找的职位
+        'title': {type: String},
+        //if boss
+        'company': {type: String},
+        'money': {type: String}
+    },
+    chat: {
+
+    }
+}
+
+//create models
+for(let m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+    getModel: function(name) {
+        return mongoose.model(name)
+    }
+}
--- server/user.js
+++ server/user.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
+
+Router.get('/list', (req, res) => {
+    User.find({}, (err, doc) => {
+        return res.json(doc)
+    })
+})
 
 Router.get('/info', (req, res) => {
     return res.json({code: 1})
