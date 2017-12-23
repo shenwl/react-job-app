baf86e8b893777e6fdb7803fc2595fd157a0d4c1
shenwl <shenweilun96@gmail.com>
2017-12-23 21:57:34 +0800

    user/register

--- server/user.js
+++ server/user.js
@@ -8,6 +8,22 @@ Router.get('/list', (req, res) => {
         return res.json(doc)
     })
 })
+//login
+Router.post('/register', (req, res) => {
+    console.log(req.body.data)
+    const {user, pwd, type} = req.body.data
+    User.findOne({user:user}, (err, doc) => {
+        if(doc) {
+            return res.json({code: 1, msg: '用户名已存在'})
+        }
+        User.create({user, pwd, type}, (err, doc) => {
+            if(err) {
+                return res.json({code: 1, msg: '数据库出错了'})
+            }
+            return res.json({code: 0})
+        })
+    })
+})
 
 Router.get('/info', (req, res) => {
     return res.json({code: 1})
