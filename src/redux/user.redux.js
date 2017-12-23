c8d1b777e8639cf2a248bd2f03bf625816b586e8
shenwl <shenweilun96@gmail.com>
2017-12-23 23:00:03 +0800

    fix errorMsg

--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -18,7 +18,7 @@ export function user(state=initState, action) {
         case REGISTER_SUCCESS:
             return {...state, msg: '',redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
         case ERROR_MSG:
-            return {...state, aiAuth: false, msg: action.msg}
+            return {...state, isAuth: false, msg: action.msg}
         default:
             return state
     }
@@ -45,7 +45,7 @@ export function register({user, pwd, repeatPwd, type}) {
                 if(res.status===200 && res.data.code===0) {
                     dispatch(registerSuccess({user, pwd, type}))
                 }else {
-                    dispatch(ERROR_MSG(res.data.msg))
+                    dispatch(errorMsg(res.data.msg))
                 }
             })
     }  
