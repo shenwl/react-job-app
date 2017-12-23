8b7074bcca8e9163b88bb817b9416f234a481779
shenwl <shenweilun96@gmail.com>
2017-12-23 16:29:21 +0800

    user/info

--- src/component/authRoute/authRoute.js
+++ src/component/authRoute/authRoute.js
@@ -5,12 +5,17 @@ class AuthRoute extends React.Component {
     componentDidMount() {
         //获取用户信息
         axios.get('/user/info')
+            .then(res => {
+                if(res.status==200) {
+                    console.log(res.data)
+                }
+            })
         //是否登录，当前url，login url不需要跳转
         //用户type（身份）
         //用户是否完善信息（选择头像，个人简介）
     }
     render() {
-        return
+        return null
     }
 }
 
