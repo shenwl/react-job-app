58db3ded0256ded768d25e0cdf73db7ff6f0e10f
shenwl <shenweilun96@gmail.com>
2018-01-08 14:36:43 +0800

    修复样式

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -58,22 +58,24 @@ class Chat extends React.Component {
                     {users[userid].name}
                 </NavBar>
 
-                {chatMsg.map(v => {
-                    const avatar = require(`../img/${users[v.from].avatar}.png`)
-                    return v.from === userid? (
-                        <List key={v._id}>
-                            <Item
-                                thumb={avatar}
-                            >{v.content}</Item>
-                        </List>
-                    ):(
-                        <List key={v._id}>
-                            <Item
-                                extra={<img src={avatar} />}
-                                className='chat-me'>{v.content}</Item>
-                        </List>
-                    )
-                })}
+                <List>
+                    {chatMsg.map(v => {
+                        const avatar = require(`../img/${users[v.from].avatar}.png`)
+                        return v.from === userid? (
+                            <List key={v._id}>
+                                <Item
+                                    thumb={avatar}
+                                >{v.content}</Item>
+                            </List>
+                        ):(
+                            <List key={v._id}>
+                                <Item
+                                    extra={<img src={avatar} />}
+                                    className='chat-me'>{v.content}</Item>
+                            </List>
+                        )
+                    })}
+                </List>
 
                 <div className="stick-footer">
                     <List>
